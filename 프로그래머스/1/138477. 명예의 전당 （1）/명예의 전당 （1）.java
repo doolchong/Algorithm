3b374eff1d4c684a5949c769475748b43d5404a7
doolchong <150253664+doolchong@users.noreply.github.com>
2024-07-18 17:22:56 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.35 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        int[] fame = new int[k + 1];
+
+        for (int i = 0; i < score.length; i++) {
+            if (i < k) {
+                fame[k - i] = score[i];
+                Arrays.sort(fame);
+                answer[i] = fame[k - i];
+            } else {
+                fame[0] = score[i];
+                Arrays.sort(fame);
+                answer[i] = fame[1];
+                fame[0] = 0;
+            }
+        }
+        return answer;
+    }
+}
