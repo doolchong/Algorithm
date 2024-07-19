2abb48da8437993821c7183e3c692cc20bbe2734
doolchong <150253664+doolchong@users.noreply.github.com>
2024-07-19 12:56:58 +0900

    [Bronze III] Title: 팩토리얼, Time: 208 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int pac = 1;
+
+        for (int i = 1; i <= N; i++) {
+            pac *= i;
+        }
+
+        System.out.println(pac);
+
+        sc.close();
+    }
+}
