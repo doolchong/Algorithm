56ab03e2035f99f27e50722e46a9caf240f57157
doolchong <150253664+doolchong@users.noreply.github.com>
2024-07-19 12:58:31 +0900

    [Bronze V] Title: Plane, Time: 208 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n1 = sc.nextInt(), k1 = sc.nextInt(), n2 = sc.nextInt(), k2 = sc.nextInt();
+
+		System.out.println(n1 * k1 + n2 * k2);
+		
+		sc.close();
+	}
+
+}
