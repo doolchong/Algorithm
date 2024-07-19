da3940aa29e7d28ba465ac361b4c768b2fbd4494
doolchong <150253664+doolchong@users.noreply.github.com>
2024-07-19 12:58:23 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6176 ms, Memory: 400016 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        BigInteger A=sc.nextBigInteger();
+        BigInteger B=sc.nextBigInteger();
+
+        System.out.println(A.multiply(B));
+
+        sc.close();
+    }
+}
