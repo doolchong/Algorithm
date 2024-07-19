72f910da263156fee12243e4c49a34d4100a699b
doolchong <150253664+doolchong@users.noreply.github.com>
2024-07-19 12:49:46 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 200 ms, Memory: 17328 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int A, B;
+
+        for (int i = 0; i < N; i++) {
+            A = sc.nextInt();
+            B = sc.nextInt();
+        }
+
+        System.out.println("비와이");
+
+        sc.close();
+    }
+}
