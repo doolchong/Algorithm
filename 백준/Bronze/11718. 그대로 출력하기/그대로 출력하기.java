350cd832f1eb6a7eacc460357186c546a56e3c6b
doolchong <150253664+doolchong@users.noreply.github.com>
2024-07-19 13:22:25 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 252 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String S;
+
+		while (sc.hasNext()) {
+			S = sc.nextLine();
+			System.out.println(S);
+		}
+		sc.close();
+	}
+
+}
