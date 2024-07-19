81f73acaaaf95929f514e1793aaa2a88466d3f82
doolchong <150253664+doolchong@users.noreply.github.com>
2024-07-19 12:52:47 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 244 ms, Memory: 18712 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int ax, ay, az;
+        int bx, by, bz;
+        int cx, cy, cz;
+
+        ax = sc.nextInt();
+        ay = sc.nextInt();
+        az = sc.nextInt();
+        cx = sc.nextInt();
+        cy = sc.nextInt();
+        cz = sc.nextInt();
+
+        bx = cx - az;
+        by = cy / ay;
+        bz = cz - ax;
+
+        System.out.println(bx + " " + by + " " + bz);
+    }
+}
