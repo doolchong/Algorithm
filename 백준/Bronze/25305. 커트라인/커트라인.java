6c2ba3bccdff8b071f56ba393d841e596d9331e1
doolchong <150253664+doolchong@users.noreply.github.com>
2024-07-25 10:16:06 +0900

    [Bronze II] Title: 커트라인, Time: 272 ms, Memory: 20756 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] score = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            score[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(score);
+
+        System.out.println(score[N - k]);
+
+    }
+}
