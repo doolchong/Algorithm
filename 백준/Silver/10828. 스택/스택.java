eba69052ec8ff4bd62477759440e455907ea233a
doolchong <150253664+doolchong@users.noreply.github.com>
2024-08-01 18:44:54 +0900

    [Silver IV] Title: 스택, Time: 376 ms, Memory: 18916 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (str.equals("push")) {
+                int X = Integer.parseInt(st.nextToken());
+                stack.push(X);
+            } else if (str.equals("pop")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.pop());
+                }
+            } else if (str.equals("size")) {
+                System.out.println(stack.size());
+            } else if (str.equals("empty")) {
+                if (stack.isEmpty()) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+            } else if (str.equals("top")) {
+                if (stack.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(stack.peek());
+                }
+            }
+        }
+    }
+}
