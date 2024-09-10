2d9fa663090e6433216edd4a925d649dcd49a31b
doolchong <150253664+doolchong@users.noreply.github.com>
2024-09-10 21:04:18 +0900

    [level 2] Title: 큰 수 만들기, Time: 4727.01 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,20 +1,18 @@
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder num = new StringBuilder(number); //StringBuilder 생성
+        StringBuilder num = new StringBuilder(number); 
         
-        int i = 0;//인덱스 변수 i
+        int i = 0;
         
        for(int j = 0; j<k; j++){
            i=0;
            int len = num.length()-1;
            while(i< len){
                if((int)num.charAt(i) < (int)num.charAt(i+1)){ 
-                   //i번째 인덱스가 i+1번째 인덱스보다 작으면, i번째 인덱스 삭제
                    num.delete(i,i+1);
                    break;
                }
                else{
-                   //그렇지 않으면, i+1번째 인덱스와 i+2번째 인덱스 비교
                    i++;
                }
            }
