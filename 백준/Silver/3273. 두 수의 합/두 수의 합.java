d38beea338d61f4c932c3f35b2423c266975ee62
doolchong <150253664+doolchong@users.noreply.github.com>
2024-12-01 19:11:08 +0900

    [Silver III] Title: 두 수의 합, Time: 840 ms, Memory: 109304 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int start = 1;
+        int end = N;
+        int sum = 0;
+        int count = 0;
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int M = scanner.nextInt();
+
+        Arrays.sort(arr);
+
+        while (start < end) {
+            sum = arr[start - 1] + arr[end - 1];
+            if (sum < M) {
+                start++;
+            } else if (sum > M) {
+                end--;
+            } else {
+                count++;
+                start++;
+                end--;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
